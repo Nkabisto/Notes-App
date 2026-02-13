43a469a4e93479bf29226000754792c04864611d
Lionel <nkabisto@gmail.com>
2026-02-13 08:39:55 +0200

    Prop Drilling---passing a prop through to multiple level of components

---
+++ src/components/Note.jsx
@@ -0,0 +1,28 @@
+const Note= ({note, deleteNote})=>{
+  const priorityColor = {
+    "High":"red",
+    "Medium": "orange",
+    "Low": "green"
+  }
+  return(
+    <div style= {{
+      borderLeftColor: priorityColor[note.priority]}}
+      className="p-4 bg-white rounded-lg shadown-md border-l-4"
+    >
+      <h3 className='text-lg font-bold'>{note.title}</h3>
+      <p className='text-sm text-gray-600'>
+        <strong>Category:</strong> {note.category}
+      </p>
+      <p className='text-sm text-gray-600'>
+        <strong>Priority:</strong> {note.priority}
+      </p>
+      <p className='mt-2'>
+        {note.description}
+      </p>
+      
+      <button onClick={()=>deleteNote(note.id)} className='mt-3 text-red-500 cursor-pointer transition hover:text-red-700'>🗑Delete 
+      </button>
+    </div>
+  )};
+
+export default Note;
--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,3 +1,5 @@
+import Note from "./Note";
+
 const NoteList = ({notes, deleteNote}) =>{
   const priorityColor = {
     "High":"red",
@@ -15,26 +17,8 @@ const NoteList = ({notes, deleteNote}) =>{
   return(
     <div className="space-y-4">
       {notes.map((note) => (
-        <div style= {{
-          borderLeftColor: priorityColor[note.priority]}}
-          key={note.id}
-          className="p-4 bg-white rounded-lg shadown-md border-l-4"
-        >
-          <h3 className='text-lg font-bold'>{note.title}</h3>
-          <p className='text-sm text-gray-600'>
-            <strong>Category:</strong> {note.category}
-          </p>
-          <p className='text-sm text-gray-600'>
-            <strong>Priority:</strong> {note.priority}
-          </p>
-          <p className='mt-2'>
-            {note.description}
-          </p>
-          
-          <button onClick={()=>deleteNote(note.id)} className='mt-3 text-red-500 cursor-pointer transition hover:text-red-700'>🗑Delete 
-          </button>
-        </div>
-    ))}
+      <Note key={note.id} note={note} deleteNote={deleteNote} />
+    ))};
   </div>
   );
 };
